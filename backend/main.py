dc0e04af624a3640fdb98df1a6174fc59930fe16
DiyaBharakhda <bharakhdadiya@gmail.com>
2026-09-22 17:27:57 +0530

    Allow Vercel frontend access

--- backend/main.py
+++ backend/main.py
@@ -107,12 +107,14 @@ def update_plant_table():
 update_plant_table()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://plant-pulse-liart.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 NOMINATIM_URL = "https://nominatim.openstreetmap.org/search"
 
 HEADERS = {
